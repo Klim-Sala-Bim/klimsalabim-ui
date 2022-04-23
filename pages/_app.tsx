a3437d405d495a35e1180360f44b11e15d6d12d3
Caroline Kabat <carolinekabat@gmail.com>
2022-04-23 17:10:31 +0200

    Add Coinbase Wallet SDK wallet connect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,21 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout }  from '../containers/Layout'
 
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+
+function getLibrary(provider: any) {
+  return new Web3Provider(provider);
+}
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Web3ReactProvider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,60 @@
 import type { NextPage } from 'next';
-import styles from '../styles/Home.module.css';
+import { useWeb3React } from '@web3-react/core';
+
+import { WalletLinkConnector } from '@web3-react/walletlink-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { InjectedConnector } from '@web3-react/injected-connector';
 
 const Home: NextPage = () => {
+  const { active, account, chainId, library, connector, activate, deactivate } =
+    useWeb3React();
+
+  const CoinbaseWallet = new WalletLinkConnector({
+    url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY_MUMBAI_TESTNET}`,
+    appName: 'Web3-react Demo',
+    supportedChainIds: [80001],
+  });
+
+  const WalletConnect = new WalletConnectConnector({
+    rpcUrl: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY_MUMBAI_TESTNET}`,
+    bridge: 'https://bridge.walletconnect.org',
+    qrcode: true,
+  });
+
+  const Injected = new InjectedConnector({
+    supportedChainIds: [80001],
+  });
+
   return (
     <>
       <h1>Klim-Sala-Bim</h1>
+      <div><strong>Connection Status: </strong>{active ? 'connected' : 'not connected'}</div>
+      <div><strong>Account:</strong> {account}</div>
+      <div><strong>Network ID:</strong> {chainId}</div>
+      <br />
+      <button
+        onClick={() => {
+          activate(CoinbaseWallet);
+        }}
+      >
+        Coinbase Wallet
+      </button>
+      <button
+        onClick={() => {
+          activate(WalletConnect);
+        }}
+      >
+        Wallet Connect
+      </button>
+      <button
+        onClick={() => {
+          activate(Injected);
+        }}
+      >
+        Metamask
+      </button>
+
+      <button onClick={deactivate}>Disconnect</button>
     </>
   );
 };
