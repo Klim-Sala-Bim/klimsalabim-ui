6bbe43a1216398ebe68acde941dbe1d577348812
Caroline Kabat <carolinekabat@gmail.com>
2022-04-23 21:34:00 +0200

    Login and styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,25 +8,14 @@ export const Header = () => {
 
   return (
     <header className={styles.header}>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>[Klim-Sala-Bim Logo]</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard">
-            <a 
-              style={{textDecoration: `${ 
-                asPath === `/dashboard`
-                  ? 'underline'
-                  : 'none'
-              }`}}
-            >Dashboard</a>
-          </Link>
-        </li>
-      </ul>
-      <button>Connect Wallet</button>
+      <img style={{ height: '100px', marginBottom: '1em'}} src="/KlimSalaBim-logo.png" />
+      <h2 style={{fontSize: '40px'}}>
+        Offset Carbon Footprint
+      </h2>
+      <h3 style={{fontSize: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <span>Take control of your carbon emissions. </span>
+        <span>Become carbon neutral and do your part for the well being of the planet.</span>
+      </h3>
     </header>
   );
 };
