b4634e2a3bd65999c11321ad70740ed9809b1e63
ramvittal <ramvittal@gmail.com>
2022-04-24 01:15:10 +0200

    Added dropdown and route maps

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,72 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
 import { Card } from '../components/Card';
 
 const Dashboard: NextPage = () => {
+
+    /** "selected" here is state variable which will hold the
+   * value of currently selected dropdown.
+   */
+     const [selected, setSelected] = useState<String>();
+  
+     /** Function that will set different values to state variable
+      * based on which dropdown is selected
+      */
+     const changeSelectOptionHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+      const value = event.target.value;
+       setSelected(value);
+     };
+
+          /** This will be used to create set of options that user will see */
+      let options = null;
+      
+      /** Setting options variable according to dropdown */
+      if (selected === "Chennai") {
+        options = "MAA";
+      } else if (selected === "Gothenburg") {
+        options = "GOT";
+      } else if (selected === "Istanbul") {
+        options = "IST";
+      } else if (selected === "Manchester") {
+        options = "MAN";
+      } else if (selected === "Zurich") {
+        options = "ZUR";
+      }
+  
   
   return (
     <>
-      <h1>Dashboard</h1>
+      <h1>Choose your Origin location below</h1>
       <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr', gridGap: '4em 3em'}}>
         <Card />
         <Card />
         <Card />
         <Card />
       </div>
+      <form>
+        <div>
+          {/** Bind changeSelectOptionHandler to onChange method of select.
+           * This method will trigger every time different
+           * option is selected.
+           */}
+          <select onChange={changeSelectOptionHandler}>
+            <option>Choose...</option>
+            <option>Chennai</option>
+            <option>Gothenburg</option>
+            <option>Istanbul</option>
+            <option>Manchester</option>
+            <option>Zurich</option>
+          </select>
+        </div>
+        <div>
+          <select>
+            {
+              /** This is where we have used our options variable */
+              options
+            }
+          </select>
+        </div>
+      </form>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
 import { InjectedConnector } from '@web3-react/injected-connector';
 
 import { KLIMSALABIM_ABI_TEST } from '../abis/KlimSalaBimABI';
+import { useState } from 'react';
 const CONTRACT_ADDRESS_TEST = '0xee8a877E6b98b4B3ad9ABbf00164103CD6789557';
 
 const Home: NextPage = () => {
@@ -22,6 +23,35 @@ const Home: NextPage = () => {
   const Injected = new InjectedConnector({
     supportedChainIds: [80001],
   });
+
+  /** "selected" here is state variable which will hold the
+* value of currently selected dropdown.
+*/
+  const [selected, setSelected] = useState<String>();
+
+  /** Function that will set different values to state variable
+   * based on which dropdown is selected
+   */
+  const changeSelectOptionHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelected(value);
+  };
+
+  /** This will be used to create set of options that user will see */
+  let option = null;
+
+  /** Setting options variable according to dropdown */
+  if (selected === "Chennai") {
+    option = "MAA";
+  } else if (selected === "Gothenburg") {
+    option = "GOT";
+  } else if (selected === "Istanbul") {
+    option = "IST";
+  } else if (selected === "Manchester") {
+    option = "MAN";
+  } else if (selected === "Zurich") {
+    option = "ZUR";
+  }
 
   return (
     <>
@@ -47,8 +77,40 @@ const Home: NextPage = () => {
       )}
       {active && (
         <div>
-          <div>DASHBOARD</div>
+          <div>Choose your Origin location below</div>
           {/* <button onClick={deactivate}>Disconnect</button> */}
+          <br/>
+          <br/>
+          <form>
+            <div>
+              <select onChange={changeSelectOptionHandler}>
+                <option>Choose...</option>
+                <option>Chennai</option>
+                <option>Gothenburg</option>
+                <option>Istanbul</option>
+                <option>Manchester</option>
+                <option>Zurich</option>
+              </select>
+            </div>
+            <br/>
+            <br/>
+            <div> Route Map</div>
+            <br/>
+            <div>
+                {
+                  /** This is where we have used our options variable */
+                  // Based on the variable we need to display the map from public folder
+                  option
+                } to AMS
+            </div>
+            <br/>
+            <br/>
+            
+            <div>
+              <img src='../Chennai.png'></img>
+            </div>
+          </form>
+
         </div>
       )}
     </>
