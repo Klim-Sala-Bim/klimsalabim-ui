02d78b15f266964fd737fa3b620fd96c2a773a03
Caroline Kabat <carolinekabat@gmail.com>
2022-04-24 05:56:29 +0200

    Mainnet stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,100 @@
 import type { NextPage } from 'next';
 import { useWeb3React } from '@web3-react/core';
-import { ethers, Contract } from 'ethers';
+import { ethers, Contract, BigNumber } from 'ethers';
+
+import Card from '../components/Card';
 
 import { WalletLinkConnector } from '@web3-react/walletlink-connector';
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
 import { InjectedConnector } from '@web3-react/injected-connector';
 
 import { KLIMSALABIM_ABI_TEST } from '../abis/KlimSalaBimABI';
+import { KLIMSALABIM_ABI_MAINNET } from '../abis/KlimSalaBimAPIPolygonMainnet0x24671685';
 import { useState } from 'react';
 const CONTRACT_ADDRESS_TEST = '0xee8a877E6b98b4B3ad9ABbf00164103CD6789557';
+const CONTRACT_ADDRESS_MAINNET = '0x246716856349a0931eefAA42be86e58C3B385a90';
 
 const Home: NextPage = () => {
   const { active, account, chainId, library, connector, activate, deactivate } =
     useWeb3React();
 
+  const cityCarbonOffsetMapping: { [key: string]: number } = {
+    Chennai: 123,
+    Gothenburg: 132,
+    Istanbul: 345,
+    Manchester: 456,
+    Zurich: 100,
+  };
+
   const CoinbaseWallet = new WalletLinkConnector({
-    url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY_MUMBAI_TESTNET}`,
+    // url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY_MUMBAI_TESTNET}`,
+    url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_KEY_MUMBAI_MAINNET}`,
     appName: 'KlimSalaBim',
-    supportedChainIds: [80001],
+    supportedChainIds: [137, 80001],
   });
 
   const Injected = new InjectedConnector({
-    supportedChainIds: [80001],
+    supportedChainIds: [137, 80001],
   });
 
   /** "selected" here is state variable which will hold the
-* value of currently selected dropdown.
-*/
+   * value of currently selected dropdown.
+   */
   const [selected, setSelected] = useState<String>();
+  const [distanceKm, setDistancetKm] = useState<Number>();
 
   /** Function that will set different values to state variable
    * based on which dropdown is selected
    */
-  const changeSelectOptionHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const changeSelectOptionHandler = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     const value = event.target.value;
     setSelected(value);
+    setDistancetKm(cityCarbonOffsetMapping[value]);
   };
 
   /** This will be used to create set of options that user will see */
   let option = null;
-  let imageSource = "";
+  let imageSource = '';
 
   /** Setting options variable according to dropdown */
-  if (selected === "Chennai") {
-    option = "MAA";
-    imageSource="../Chennai.png";
-  } else if (selected === "Gothenburg") {
-    option = "GOT";
-    imageSource="../Gothenburg.png";
-  } else if (selected === "Istanbul") {
-    option = "IST";
-    imageSource="../Istanbul.png";
-  } else if (selected === "Manchester") {
-    option = "MAN";
-    imageSource="../Manchester.png";
-  } else if (selected === "Zurich") {
-    option = "ZUR";
-    imageSource="../Zurich.png";
+  if (selected === 'Chennai') {
+    option = 'MAA';
+    imageSource = '../Chennai.png';
+  } else if (selected === 'Gothenburg') {
+    option = 'GOT';
+    imageSource = '../Gothenburg.png';
+  } else if (selected === 'Istanbul') {
+    option = 'IST';
+    imageSource = '../Istanbul.png';
+  } else if (selected === 'Manchester') {
+    option = 'MAN';
+    imageSource = '../Manchester.png';
+  } else if (selected === 'Zurich') {
+    option = 'ZUR';
+    imageSource = '../Zurich.png';
   }
+
+  const sendMatic =  async () => {
+    const contract = new Contract(
+      CONTRACT_ADDRESS_MAINNET,
+      KLIMSALABIM_ABI_MAINNET,
+      library.getSigner()
+    );
+
+    const wallet = await library.getSigner().getAddress();
+
+    const res = await contract.compensateSingleParticipantTravel(
+      'Gothenburg',
+      1337,
+      10000000000000000,
+      0,
+      { sender: wallet, value: 54882117495512018 }
+    );
+    
+    console.log(res);
+  };
 
   return (
     <>
@@ -82,13 +119,30 @@ const Home: NextPage = () => {
         </div>
       )}
       {active && (
-        <div>
-          <div>Choose your Origin location below</div>
-          {/* <button onClick={deactivate}>Disconnect</button> */}
-          <br/>
-          <br/>
-          <form>
+        <div style={{ border: '1px solid red' }}>
+          {option ? (
+            <div
+              style={{
+                display: 'grid',
+                gridGap: '30px',
+                gridTemplateColumns: '1fr 1fr',
+              }}
+            >
+              <div>
+                <h3>Route Map</h3> {option} to AMS
+                <img
+                  style={{ borderRadius: '24px', maxWidth: '400px' }}
+                  src={imageSource}
+                ></img>
+              </div>
+              <div>
+                distance: {distanceKm}
+                <button onClick={sendMatic}>Send MATIC</button>
+              </div>
+            </div>
+          ) : (
             <div>
+              <h2>Choose your Origin location below</h2>
               <select onChange={changeSelectOptionHandler}>
                 <option>Choose...</option>
                 <option>Chennai</option>
@@ -98,23 +152,7 @@ const Home: NextPage = () => {
                 <option>Zurich</option>
               </select>
             </div>
-            <br/>
-            <br/>
-            <div> Route Map</div>
-            <br/>
-            <div>
-                {
-                  /** This is where we have used our options variable */
-                  // Based on the variable we need to display the map from public folder
-                  option
-                } to AMS
-            </div>
-            <br/>
-            <div>
-              <img src={imageSource}></img>
-            </div>
-          </form>
-
+          )}
         </div>
       )}
     </>
